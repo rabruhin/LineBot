b0849b56e2ccd3b0e400f7cd40dff19079fe50f6
rabruhin <167390949+rabruhin@users.noreply.github.com>
2024-05-23 10:29:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ import traceback
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_bot_api = LineBotApi(os.getenv('muViR65ZYVJhUv8tf0PVI8EbMSTOl44Cdz8XxNa+r69auP0twVEWLKTKxv0SjFyYJzW4cPonW9WR2imIvRNN01FxiBtRbz+O5ot+gFUNsb9YBD3m5SQy6RYiU+eZlSLMi4KRZZW1aPGPQ/4gcZJGlQdB04t89/1O/w1cDnyilFU='))
 # Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+handler = WebhookHandler(os.getenv('4aeec8dd13bccc6f8eb66415a3aa21b9'))
 
 def QA_response(text):
     client = QuestionAnsweringClient(endpoint, credential)
